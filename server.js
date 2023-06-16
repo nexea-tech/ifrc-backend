7f4568612079f4f60bb1ed8197c65305381f4438
apikolll <afiqnrzm@hotmail.com>
2023-06-16 11:42:12 +0800

    First commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+app.listen(process.env.PORT || 6001, () => {
+  console.log(`Listening to port ${process.env.PORT}...`);
+});
