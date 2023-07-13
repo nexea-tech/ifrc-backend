f7d7cdd6ebee3affd4df167b4b4ba8d8b1077473
afiqnexea <afiqnrzm@hotmail.com>
2023-07-13 23:38:35 +0800

    add register route

--- routes/registerRoute.js
+++ routes/registerRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { getRegister } = require("../controllers/registerController");
 
+
 router.route("/").get(getRegister);
 
 module.exports = router;
